ba62da20271333117b68b183ed6949b6dd3c1089
Edwin <Edwinallen94@aol.com>
2024-05-01 20:09:52 -0400

    first draft of project

---
+++ NiaCollab/script.js
@@ -0,0 +1,63 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const darkModeToggle = document.getElementById("darkModeToggle");
+  const body = document.body;
+
+  darkModeToggle.addEventListener("change", function () {
+    if (this.checked) {
+      body.classList.add("dark-mode");
+      localStorage.setItem("theme", "dark");
+    } else {
+      body.classList.remove("dark-mode");
+      localStorage.setItem("theme", "light");
+    }
+  });
+
+  // Check local storage for theme preference
+  const currentTheme = localStorage.getItem("theme");
+  if (currentTheme === "dark") {
+    body.classList.add("dark-mode");
+    darkModeToggle.checked = true;
+  }
+});
+
+// Get references to the tab buttons and tab contents
+const tabButtons = document.querySelectorAll('[role="tab"]');
+const tabContents = document.querySelectorAll('[role="tabpanel"]');
+
+// Hide all tab contents initially
+tabContents.forEach((tabContent) => {
+  tabContent.classList.add("hidden");
+});
+
+// Add click event listener to each tab button
+tabButtons.forEach((tabButton) => {
+  tabButton.addEventListener("click", function (event) {
+    // Prevent default behavior of button clicks
+    event.preventDefault();
+
+    // Get the ID of the clicked tab button
+    const tabId = event.target.getAttribute("aria-controls");
+
+    // Hide all tab contents
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.add("hidden");
+    });
+
+    // Show the corresponding tab content based on the clicked tab button
+    const activeTabContent = document.getElementById(tabId);
+    activeTabContent.classList.remove("hidden");
+  });
+});
+
+// Read more toggle
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   var readMoreLinks = document.querySelectorAll(".read-more");
+//   readMoreLinks.forEach(function (link) {
+//     link.addEventListener("click", function (e) {
+//       e.preventDefault();
+//       var fullMessage = e.target.parentElement.nextElementSibling;
+//       fullMessage.classList.toggle("hidden");
+//     });
+//   });
+// });
